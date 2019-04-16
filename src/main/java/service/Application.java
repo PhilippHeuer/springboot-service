e03e11b7c1eec45b25997fc7f4136923cd730a32
Philipp Heuer <git@philippheuer.me>
2019-04-16 23:12:28 +0200

    feature: add new events and error module

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -1,12 +1,14 @@
 package service;
 
+import com.github.philippheuer.events4j.Events4JModule;
 import com.github.philippheuer.swaggerwebflux.WebFluxSwaggerModule;
+import com.github.philippheuer.webfluxerror.WebFluxErrorModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(WebFluxSwaggerModule.class)
+@Import({WebFluxSwaggerModule.class, Events4JModule.class, WebFluxErrorModule.class})
 public class Application {
 
     public static void main(String[] args) {
