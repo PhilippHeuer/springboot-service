863b785cb899f3896d9365cb36c07ce11e742a26
Philipp Heuer <git@philippheuer.me>
2019-06-12 23:08:17 +0200

    chore: update to v0.0.17

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -2,13 +2,19 @@ package service;
 
 import com.github.philippheuer.events4j.Events4JModule;
 import com.github.philippheuer.swaggerwebflux.WebFluxSwaggerModule;
+import com.github.philippheuer.tracing.TracingModule;
 import com.github.philippheuer.webfluxerror.WebFluxErrorModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({WebFluxSwaggerModule.class, Events4JModule.class, WebFluxErrorModule.class})
+@Import({
+    Events4JModule.class,
+    TracingModule.class,
+    WebFluxErrorModule.class,
+    WebFluxSwaggerModule.class,
+})
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/service/controller/GreetingController.java
+++ src/main/java/service/controller/GreetingController.java
@@ -1,21 +1,39 @@
 package service.controller;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.log.Fields;
+import io.opentracing.tag.Tags;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/v1/greeting")
+@RequestMapping(value="/v1", produces = {
+    MediaType.APPLICATION_JSON_UTF8_VALUE
+})
 @Slf4j
 public class GreetingController {
+
+    @Autowired
+    private Tracer tracer;
+
+    @Autowired
+    private MeterRegistry meterRegistry;
 
     /**
      * Greetings
@@ -23,12 +41,35 @@ public class GreetingController {
      * @param name
      * @return
      */
-    @RequestMapping("/print")
-    public Mono<Map> greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping("/hello")
+    @Timed
+    public ResponseEntity<Mono<Map>> greeting(@RequestParam(value="name", defaultValue="World") String name) {
         Map<String, String> result = new HashMap<>();
         result.put("id", "1");
         result.put("text", String.format("Hello, %s!", name));
 
-        return Mono.just(result);
+        Counter counter = meterRegistry.counter("test");
+        counter.increment(2);
+
+        // partial span to track independent operations
+        Span requestSpan = tracer.activeSpan();
+        Span span = tracer.buildSpan(String.format("%s.%s.%s", getClass().getSimpleName(), new Throwable().getStackTrace()[0].getMethodName(), "test")).asChildOf(requestSpan).start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+
+
+        } catch(Exception ex) {
+            Tags.ERROR.set(span, true);
+
+
+            //span.log(Map.of(Fields.EVENT, "error", Fields.ERROR_OBJECT, ex, Fields.MESSAGE, ex.getMessage()));
+        } finally {
+            span.finish();
+        }
+
+
+
+
+        return ResponseEntity.ok().body(Mono.just(result));
     }
+
 }
